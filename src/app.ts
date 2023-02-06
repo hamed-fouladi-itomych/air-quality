dabaee69c260c01dca8910593a5c221de02803e4
hamed-fouladi-itomych <hamed.fouladi@itomy.ch>
2023-02-06 14:00:29 +0200

    add cors

--- src/app.ts
+++ src/app.ts
@@ -2,13 +2,24 @@ import * as dotenv from 'dotenv';
 import express from 'express';
 import initRoutes from './init/init.routes';
 import errorHandler from './middlewares/error-handler';
+import cors from 'cors';
 
 dotenv.config();
 
 const app = express();
 
+const options = {
+  allowedHeaders: '*',
+  credentials: true,
+  methods: '*',
+  origin: '*',
+  preflightContinue: false,
+  optionsSuccessStatus: 200,
+};
+
 app.use(express.json());
 initRoutes(app);
+app.use(cors(options));
 app.use(errorHandler);
 
 export default app;
